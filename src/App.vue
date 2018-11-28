6a6aa2ba17848573a00fafa0ad1088f767ae7556
jes <jes.shaw@126.com>
2018-11-28 10:58:48 +0800

    add router vuex bulma font awesome icons etc.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,124 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <nav class="navbar is-transparent is-fixed-top" role="navigation" aria-label="main navigation">
+            <div class="container">
+                <div class="navbar-brand">
+                    <router-link to="/" class="navbar-item" @click.native="triggerNav">
+                        <font-awesome-icon icon="cat" size="2x"/>
+                    </router-link>
+                    <a role="button" class="navbar-burger burger" :class="{'is-active':navIsActive}" aria-label="menu"
+                       aria-expanded="false"
+                       @click="triggerNav">
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
+                </div>
+
+                <div class="navbar-menu" :class="{'is-active':navIsActive}">
+                    <div class="navbar-start">
+                        <router-link to="/" class="navbar-item" @click.native="triggerNav">
+                            <span class="icon has-text-info"><font-awesome-icon icon="home" fixed-width/></span>
+                            <span>首页</span>
+                        </router-link>
+                        <router-link to="/about" class="navbar-item" @click.native="triggerNav">
+                            <span class="icon has-text-warning">
+                                <font-awesome-icon icon="building" fixed-width/></span>
+                            <span>关于我们</span>
+                        </router-link>
+                        <div class="navbar-item has-dropdown is-hoverable">
+                            <a class="navbar-link">
+                                更多
+                            </a>
+
+                            <div class="navbar-dropdown">
+                                <a class="navbar-item">
+                                     <span class="icon has-text-danger">
+                                <font-awesome-icon icon="user-md" fixed-width/></span>
+                                    <span>工作机会</span>
+                                </a>
+                                <a class="navbar-item">
+
+                                     <span class="icon has-text-success">
+                                <font-awesome-icon icon="phone" fixed-width/></span>
+                                    <span>联系我们</span>
+
+                                </a>
+                                <hr class="navbar-divider">
+                                <a class="navbar-item">
+
+
+                                     <span class="icon has-text-grey-dark">
+                                <font-awesome-icon icon="question" fixed-width/></span>
+                                    <span>建议</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="navbar-end">
+                        <div class="navbar-item">
+                            <div class="buttons">
+                                <a class="button is-info">
+                                    <span class="icon">
+                                        <font-awesome-icon icon="user-plus" fixed-width/>
+                                    </span>
+                                    <span>
+                                        注册
+                                    </span>
+                                </a>
+                                <a class="button is-light">
+                                    <span class="icon">
+                                        <font-awesome-icon icon="sign-in-alt" fixed-width/>
+                                    </span>
+                                    <span>
+                                        登录
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <section class="hero is-primary is-bold is-info">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title">
+                        Primary bold title
+                    </h1>
+                    <h2 class="subtitle">
+                        Primary bold subtitle
+                    </h2>
+                </div>
+            </div>
+        </section>
+        <router-view/>
+        <footer class="footer">
+            <div class="content has-text-centered">
+                <p>
+                    <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is
+                    licensed
+                    <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+                    is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+                </p>
+            </div>
+        </footer>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        data() {
+            return {navIsActive: false}
+        },
+        methods: {
+            triggerNav: function () {
+                this.navIsActive = !this.navIsActive;
+            }
+        }
+    }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+    @import "~bulma/bulma";
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,72 @@
+<template>
+    <section class="section">
+        <div class="container">
+            <!-- Main container -->
+            <nav class="columns">
+                <!-- Left side -->
+                <div class="column is-narrow is-hidden-mobile">
+                    <aside class="menu" style="width: 200px;">
+                        <p class="menu-label">
+                            General
+                        </p>
+                        <ul class="menu-list">
+                            <li><a>Dashboard</a></li>
+                            <li><a>Customers</a></li>
+                        </ul>
+                        <p class="menu-label">
+                            Administration
+                        </p>
+                        <ul class="menu-list">
+                            <li><a>Team Settings</a></li>
+                            <li>
+                                <a class="is-active">Manage Your Team</a>
+                                <ul>
+                                    <li><a>Members</a></li>
+                                    <li><a>Plugins</a></li>
+                                    <li><a>Add a member</a></li>
+                                </ul>
+                            </li>
+                            <li><a>Invitations</a></li>
+                            <li><a>Cloud Storage Environment Settings</a></li>
+                            <li><a>Authentication</a></li>
+                        </ul>
+                        <p class="menu-label">
+                            Transactions
+                        </p>
+                        <ul class="menu-list">
+                            <li><a>Payments</a></li>
+                            <li><a>Transfers</a></li>
+                            <li><a>Balance</a></li>
+                        </ul>
+                    </aside>
+                </div>
+
+                <!-- Right side -->
+                <div class="column">
+
+                    <HelloWorld msg="Welcome to Your Vue.js App"/>
+                </div>
+                <div class="column is-narrow">
+                    <div class="notification" style="width: 300px;">
+                        Lorem ipsum dolor sit amet, consectetur
+                        adipiscing elit lorem ipsum dolor. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Sit amet,
+                        consectetur adipiscing elit
+                    </div>
+                </div>
+            </nav>
+
+        </div>
+    </section>
+</template>
+
+<script>
+    // @ is an alias to /src
+    import HelloWorld from '@/components/HelloWorld.vue'
+
+    export default {
+        name: 'home',
+        components: {
+            HelloWorld
+        }
+    }
+</script>

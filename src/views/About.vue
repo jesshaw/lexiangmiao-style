f44edef620eb400e7dec80e3cb629fb33b7fb5e8
jes <jesshaw@126.com>
2018-12-05 00:29:54 +0800

    question detail layput

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,216 +1,140 @@
 <template>
-  <div>
-    <section class="section main-content">
-      <div class="container">
-        <!--<div class="tile is-ancestor">-->
-          <!--<div class="tile is-parent">-->
-            <!--<article class="tile is-child notification is-white">-->
-              <!--&lt;!&ndash;<div class="content">&ndash;&gt;-->
-                <!--&lt;!&ndash;<p class="title">Tall tile</p>&ndash;&gt;-->
-                <!--&lt;!&ndash;<p class="subtitle">With even more content</p>&ndash;&gt;-->
-                <!--&lt;!&ndash;<div class="content">&ndash;&gt;-->
-                  <!--&lt;!&ndash;&lt;!&ndash; Content &ndash;&gt;&ndash;&gt;-->
-                <!--&lt;!&ndash;</div>&ndash;&gt;-->
-              <!--&lt;!&ndash;</div>&ndash;&gt;-->
-
-              <!--<aside class="menu">-->
-                <!--<ul class="menu-list">-->
-                  <!--<li><a>首页</a></li>-->
-                  <!--<li>-->
-                    <!--<a class="is-disabled">导航</a>-->
-                    <!--<ul>-->
-                      <!--<li><a class="is-active">问题</a></li>-->
-                      <!--<li><a>标签</a></li>-->
-                      <!--<li><a>用户</a></li>-->
-                    <!--</ul>-->
-                  <!--</li>-->
-                <!--</ul>-->
-              <!--</aside>-->
-            <!--</article>-->
-          <!--</div>-->
-          <!--<div class="tile is-vertical is-10">-->
-
-            <!--<div class="tile is-parent">-->
-              <!--<article class="tile is-child notification is-white">-->
-
-                <!--<div class="columns">-->
-                <!--<div class="column is-four-fifths ">-->
-                  <!--<p class="title">When I am executing Scenario Outline it is executiOutline it is executin-->
-                    <!--g multiple times from given statements(JVM Cuumber)Outline it is executing multiple times-->
-
-                  <!--</p>-->
-                <!--</div>-->
-                <!--<div class="column has-text-right"><a class="button is-link">立即提问</a></div>-->
-                <!--</div>-->
-
-                <!--&lt;!&ndash;<p class="subtitle">Aligned with the right tile</p>&ndash;&gt;-->
-                <!--&lt;!&ndash;<div class="content">&ndash;&gt;-->
-                  <!--&lt;!&ndash;&lt;!&ndash; Content &ndash;&gt;&ndash;&gt;-->
-                <!--&lt;!&ndash;</div>&ndash;&gt;-->
-
-              <!--</article>-->
-            <!--</div>-->
-            <!--<div class="tile">-->
-              <!--<div class="tile is-parent is-9">-->
-                <!--<article class="tile is-child notification is-white">-->
-
-                  <!--<article class="media">-->
-                    <!--<figure class="media-left">-->
-                      <!--<p class="image is-64x64">-->
-                        <!--<img src="https://bulma.io/images/placeholders/128x128.png">-->
-                      <!--</p>-->
-                    <!--</figure>-->
-                    <!--<div class="media-content">-->
-                      <!--<div class="content">-->
-                        <!--<p>-->
-                          <!--<strong>Barbara Middleton</strong>-->
-                          <!--<br>-->
-                          <!--Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis porta eros lacus, nec ultricies elit blandit non. Suspendisse pellentesque mauris sit amet dolor blandit rutrum. Nunc in tempus turpis.-->
-                          <!--<br>-->
-                          <!--<small><a>Like</a> · <a>Reply</a> · 3 hrs</small>-->
-                        <!--</p>-->
-                      <!--</div>-->
-                    <!--</div>-->
-                  <!--</article>-->
-                  <!--<article class="media">-->
-                    <!--<figure class="media-left">-->
-                      <!--<p class="image is-64x64">-->
-                        <!--<img src="https://bulma.io/images/placeholders/128x128.png">-->
-                      <!--</p>-->
-                    <!--</figure>-->
-                    <!--<div class="media-content">-->
-                      <!--<div class="field">-->
-                        <!--<p class="control">-->
-                          <!--<textarea class="textarea" placeholder="回答内容..."></textarea>-->
-                        <!--</p>-->
-                      <!--</div>-->
-                      <!--<div class="field">-->
-                        <!--<p class="control">-->
-                          <!--<button class="button is-info">提交</button>-->
-                        <!--</p>-->
-                      <!--</div>-->
-                    <!--</div>-->
-                  <!--</article>-->
-                <!--</article>-->
-              <!--</div>-->
-              <!--<div class="tile is-parent is-vertical">-->
-                <!--<article class="tile is-child notification is-white">-->
-                  <!--<nav class="panel">-->
-                    <!--<p class="panel-heading">sdfsafsaf</p>-->
-                    <!--<p class="panel-block">-->
-                      <!--Lorem ipsum dolor sit amet, consectetur-->
-                      <!--adipiscing elit lorem ipsum dolor. Lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor. Lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor.-->
-                    <!--</p>-->
-                  <!--</nav>-->
-                <!--</article>-->
-              <!--</div>-->
-            <!--</div>-->
-          <!--</div>-->
-
-        <!--</div>-->
-        <!-- Main container -->
-        <nav class="columns">
-          <!-- Left side -->
-          <div class="column is-narrow is-hidden-mobile main-content-aside">
-            <aside class="menu">
-              <ul class="menu-list">
-                <li><a>首页</a></li>
-                <li>
-                  <a class="is-disabled">导航</a>
-                  <ul>
-                    <li><a class="is-active">问题</a></li>
-                    <li><a>标签</a></li>
-                    <li><a>用户</a></li>
-                  </ul>
-                </li>
-              </ul>
-            </aside>
-          </div>
-
-          <!-- Right side -->
-          <div class="column">
-            <!--<div class="columns">-->
-              <!--<div class="column is-full">-->
-                <!--When I am executing Scenario Outline it is executin-->
-                <!--g multiple times from given statements(JVM Cuumber)Outline it is executing multiple times-->
-              <!--</div>-->
-              <!--<div class="column has-text-right"><a class="button is-link">立即提问</a></div>-->
-
-            <!--</div>-->
-            <div class="columns is-multiline">
-              <div class="column is-full">
-                <!--<div class="level">-->
-                  <!--<div class="level-left">-->
-                    <!--<p class="subtitle is-3">-->
-                    <!--When I am executing Scenario Outline it is executiOutline it is executin-->
-                    <!--g multiple times from given statements(JVM Cuumber)Outline it is executing multiple times-->
-                    <!--</p>-->
-                  <!--</div>-->
-                  <!--<div class="level-right has-text-right"><a class="button is-link">立即提问</a></div>-->
-                <!--</div>-->
-                  <div class="columns">
-                    <div class="column is-10">
-                      <p class="title">
-                        When I am executing Scenario Outline it is executin
-                        g multiple times from given statements(JVM Cuumber)Outline it is executing multiple times</p>
+    <div>
+        <section class="section main-content">
+            <div class="container">
+                <!-- Main container -->
+                <nav class="columns">
+                    <!-- Left side -->
+                    <div class="column is-narrow is-hidden-mobile main-content-aside">
+                        <aside class="menu">
+                            <ul class="menu-list">
+                                <li><a>首页</a></li>
+                                <li>
+                                    <a class="is-disabled">导航</a>
+                                    <ul>
+                                        <li><a class="is-active">问题</a></li>
+                                        <li><a>标签</a></li>
+                                        <li><a>用户</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </aside>
                     </div>
-                    <div class="column has-text-right"><a class="button is-link">立即提问</a></div>
-                  </div>
-              </div>
-              <div class="column">
-                <article class="media">
-                  <figure class="media-left">
-                    <p class="image is-64x64">
-                      <img src="https://bulma.io/images/placeholders/128x128.png">
-                    </p>
-                  </figure>
-                  <div class="media-content">
-                    <div class="content">
-                      <p>
-                        <strong>Barbara Middleton</strong>
-                        <br>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis porta eros lacus, nec ultricies elit blandit non. Suspendisse pellentesque mauris sit amet dolor blandit rutrum. Nunc in tempus turpis.
-                        <br>
-                        <small><a>Like</a> · <a>Reply</a> · 3 hrs</small>
-                      </p>
+
+                    <!-- Right side -->
+                    <div class="column">
+                        <div class="columns is-multiline">
+                            <div class="column is-full">
+                                <div class="columns">
+                                    <div class="column is-10">
+                                        <p class="title">
+                                            When I am executing Scenario Outline it is executin
+                                            g multiple times from given statements(JVM Cuumber)Outline it is executing
+                                            multiple times</p>
+                                    </div>
+                                    <div class="column has-text-right"><a class="button is-link">立即提问</a></div>
+                                </div>
+                            </div>
+                            <div class="column">
+                                <article class="media">
+                                    <figure class="media-left">
+                                        <div class=" vote has-text-centered">
+                                            <a class="icon has-text-grey-lighter" title="经常碰到此类问题">
+                                                <font-awesome-icon icon="caret-up" size="3x"/>
+                                            </a>
+                                            <span class="vote-count-post">10</span>
+                                            <a class="icon has-text-grey-lighter" title="从未经常碰到此类问题">
+                                                <font-awesome-icon icon="caret-down" size="3x"/>
+                                            </a>
+                                            <a class="icon has-text-grey-lighter">
+                                                <font-awesome-icon icon="star" size="2x"/>
+                                            </a>
+                                        </div>
+                                    </figure>
+                                    <div class="media-content">
+                                        <div class="content">
+                                            <p>
+                                                <strong>Barbara Middleton</strong>
+                                                <br>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis porta eros
+                                                lacus, nec ultricies elit blandit non. Suspendisse pellentesque mauris
+                                                sit amet dolor blandit rutrum. Nunc in tempus turpis.
+                                                <br>
+                                                <small><a>Like</a> · <a>Reply</a> · 3 hrs</small>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </article>
+                                <article class="media">
+                                    <figure class="media-left">
+                                        <div class=" vote has-text-centered">
+                                            <a class="icon has-text-grey-lighter" title="经常碰到此类问题">
+                                                <font-awesome-icon icon="caret-up" size="3x"/>
+                                            </a>
+                                            <span class="vote-count-post">1</span>
+                                            <a class="icon has-text-grey-lighter" title="从未经常碰到此类问题">
+                                                <font-awesome-icon icon="caret-down" size="3x"/>
+                                            </a>
+                                        </div>
+                                    </figure>
+                                    <div class="media-content">
+                                        <div class="content">
+                                            <p>
+                                                <strong>Barbara Middleton</strong>
+                                                <br>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis porta eros
+                                                lacus, nec ultricies elit blandit non. Suspendisse pellentesque mauris
+                                                sit amet dolor blandit rutrum. Nunc in tempus turpis.
+                                                <br>
+                                                <small><a>Like</a> · <a>Reply</a> · 3 hrs</small>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </article>
+                                <article class="media">
+                                    <div class="media-content">
+                                        <div class="field">
+                                            <p class="control">
+                                                <textarea class="textarea" placeholder="回答内容..."></textarea>
+                                            </p>
+                                        </div>
+                                        <div class="field">
+                                            <p class="control">
+                                                <button class="button is-info">提交</button>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </article>
+                            </div>
+                            <div class="column is-narrow is-one-fifth">
+                                <nav class="panel">
+                                    <p class="panel-heading">sdfsafsaf</p>
+                                    <p class="panel-block">
+                                        Lorem ipsum dolor sit amet, consectetur
+                                        adipiscing elit lorem ipsum dolor. Lorem ipsum dolor sit amet, consectetur
+                                        adipiscing elit lorem ipsum dolor. Lorem ipsum dolor sit amet, consectetur
+                                        adipiscing elit lorem ipsum dolor.
+                                        Lorem ipsum dolor sit amet, consectetur
+                                        Lorem ipsum dolor sit amet, consectetur
+                                    </p>
+                                </nav>
+                            </div>
+                        </div>
                     </div>
-                  </div>
-                </article>
-                <article class="media">
-                  <figure class="media-left">
-                    <p class="image is-64x64">
-                      <img src="https://bulma.io/images/placeholders/128x128.png">
-                    </p>
-                  </figure>
-                  <div class="media-content">
-                    <div class="field">
-                      <p class="control">
-                        <textarea class="textarea" placeholder="回答内容..."></textarea>
-                      </p>
-                    </div>
-                    <div class="field">
-                      <p class="control">
-                        <button class="button is-info">提交</button>
-                      </p>
-                    </div>
-                  </div>
-                </article>
-              </div>
-              <div class="column is-narrow is-one-fifth">
-                <nav class="panel">
-                  <p class="panel-heading">sdfsafsaf</p>
-                  <p class="panel-block">
-                    Lorem ipsum dolor sit amet, consectetur
-                    adipiscing elit lorem ipsum dolor. Lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor. Lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor.
-                    Lorem ipsum dolor sit amet, consectetur
-                    Lorem ipsum dolor sit amet, consectetur
-                  </p>
                 </nav>
-              </div>
             </div>
-          </div>
-        </nav>
-      </div>
-    </section>
-  </div>
+        </section>
+    </div>
 </template>
+
+
+<style lang="scss">
+    .vote{
+        width: 32px;
+    }
+    .vote-count-post{
+        display: block;
+        font-size: 20px;
+        margin: 0 0 3px;
+    }
+</style>
